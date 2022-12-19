a903243f60054eea50f6933117cc30e5559c1969
qaiserjofficial <qaiserjavedofficial@gmail.com>
2022-12-19 21:53:45 +0500

    Adding Dynamic Data using Props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,14 @@
 import "./ExpenseItem.css";
-function ExpenseItem() {
+function ExpenseItem(props) {
+  // const expenseDate = new Date(2021, 2, 28);
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 3000;
   return (
     <div className="expense-item">
-      <div>December 19 2022</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price ">Pkr. 34000 </div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price ">Rupee:{props.amount}</div>
       </div>
     </div>
   );
